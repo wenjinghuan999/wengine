5786b45c59a85d0c7ed0b1d591574fd83dde5435
wensir <wenjinghuan999@126.com>
2022-01-01 14:18:10 +0800

    add unit test for queue allocation

--- test/gfx/gfx-test.cpp
+++ test/gfx/gfx-test.cpp
@@ -3,28 +3,115 @@
 
 #include <fmt/format.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include "gfx/gfx.h"
+#include "gfx-private.h"
 
-namespace glm {
+#include <sstream>
 
-doctest::String toString(const glm::vec4& v) {
-    return fmt::format("({}, {}, {}, {})", v.x, v.y, v.z, v.w).c_str();
+namespace {
+
+class PhysicalDevice_Test : public wg::PhysicalDevice {
+public:
+    struct Impl : public wg::PhysicalDevice::Impl {
+        Impl() : wg::PhysicalDevice::Impl() {}
+    };
+};
+
+} // unnamed namespace
+
+namespace std {
+
+doctest::String toString(const std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES>& queue_index_remap) {
+    std::stringstream ss;
+    bool first_outer = true;
+    for (auto&& queue_index_remap_family : queue_index_remap) {
+        ss << fmt::format("{}[", first_outer ? "" : " ");
+        first_outer = false;
+
+        bool first = true;
+        for (auto&& remap : queue_index_remap_family) {
+            ss << fmt::format("{}({},{})", first ? "" : " ", remap.first, remap.second);
+            first = false;
+        }
+        ss << "]";
+    }
+    return ss.str().c_str();
 }
 
-}
+} // namespace std
 
-TEST_CASE("glm") {
-    glm::mat4 matrix(
-        1.f, 2.f, 3.f, 4.f,
-        5.f, 6.f, 7.f, 8.f,
-        1.f, 2.f, 3.f, 4.f,
-        5.f, 6.f, 7.f, 8.f
-        );
-    glm::vec4 vec(1.f, 2.f, 3.f, 4.f);
-    glm::vec4 result = matrix * vec;
-    glm::vec4 answer(34.f, 44.f, 54.f, 64.f);
-    CHECK(result == answer);
+TEST_CASE("allocate queues" * doctest::timeout(1)) {
+    PhysicalDevice_Test::Impl impl;
+
+    SUBCASE("compact") {
+        impl.num_queues = { 1 };
+        impl.queue_supports = { 0b1111 };
+
+        std::array<int, wg::gfx_queues::NUM_QUEUES> required =
+            { 1, 1, 1, 1 };
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap; 
+        impl.allocateQueues(required, {}, queue_index_remap);
+
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap_test = {
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 } }, 
+        }; 
+        CHECK_EQ(queue_index_remap, queue_index_remap_test);
+    }
+    
+    SUBCASE("rich") {
+        impl.num_queues = { 16, 2, 4 };
+        impl.queue_supports = { 0b1111, 0b0010, 0b0011 };
+
+        std::array<int, wg::gfx_queues::NUM_QUEUES> required =
+            { 1, 1, 1, 1 };
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap; 
+        impl.allocateQueues(required, {}, queue_index_remap);
+
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap_test = {
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 1 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 2 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 3 } }, 
+        }; 
+        CHECK_EQ(queue_index_remap, queue_index_remap_test);
+    }
+
+    SUBCASE("adequete") {
+        impl.num_queues = { 1, 1, 1, 1 };
+        impl.queue_supports = { 0b1111, 0b1111, 0b1111, 0b1111 };
+
+        std::array<int, wg::gfx_queues::NUM_QUEUES> required =
+            { 1, 1, 1, 1 };
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap; 
+        impl.allocateQueues(required, {}, queue_index_remap);
+
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap_test = {
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 1, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 2, 0 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 3, 0 } }, 
+        }; 
+        CHECK_EQ(queue_index_remap, queue_index_remap_test);
+    }
+
+    SUBCASE("greedy") {
+        impl.num_queues = { 16, 2, 4 };
+        impl.queue_supports = { 0b1111, 0b0010, 0b0011 };
+
+        std::array<int, wg::gfx_queues::NUM_QUEUES> required =
+            { 16, 16, 4, 4 };
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap; 
+        impl.allocateQueues(required, {}, queue_index_remap);
+
+        std::array<std::vector<std::pair<uint32_t, uint32_t>>, wg::gfx_queues::NUM_QUEUES> queue_index_remap_test = {
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 0, 6 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 7 }, { 0, 8 }, { 0, 9 }, { 0, 10 }, { 0, 11 }, { 0, 12 }, { 0, 13 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 14 } }, 
+            std::vector<std::pair<uint32_t, uint32_t>>{ { 0, 15 } }, 
+        }; 
+        CHECK_EQ(queue_index_remap, queue_index_remap_test);
+    }
 }
---
+++ test/platform/platform-test.cpp
@@ -0,0 +1,28 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+#include <fmt/format.h>
+
+#include "platform.inc"
+#include "common/math.h"
+
+namespace glm {
+
+doctest::String toString(const glm::vec4& v) {
+    return fmt::format("({}, {}, {}, {})", v.x, v.y, v.z, v.w).c_str();
+}
+
+}
+
+TEST_CASE("glm") {
+    glm::mat4 matrix(
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f
+        );
+    glm::vec4 vec(1.f, 2.f, 3.f, 4.f);
+    glm::vec4 result = matrix * vec;
+    glm::vec4 answer(34.f, 44.f, 54.f, 64.f);
+    CHECK(result == answer);
+}
